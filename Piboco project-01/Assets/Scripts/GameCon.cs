0e3dd722898a7ca7ae788aec111fadcba931024c
GameDev-Mark <gamedev.markd@gmail.com>
2021-01-02 20:31:19 +0100

    Script input name update

--- Piboco project-01/Assets/Scripts/GameCon.cs
+++ Piboco project-01/Assets/Scripts/GameCon.cs
@@ -16,9 +16,13 @@ public class GameCon : MonoBehaviour
     {
         if(Input.GetMouseButton(0))
         {
-            if (Screen.width > Screen.width / 2)
+            if (Input.mousePosition.x > Screen.width / 2)
             {
                 Debug.Log("Touch and drag right side of screen");
+            }
+            else if (Input.mousePosition.x < Screen.width / 2)
+            {
+                Debug.Log("Touch and drag left side of screen");
             }
         }
     }
--- Piboco project-01/Assets/Scripts/InputName.cs
+++ Piboco project-01/Assets/Scripts/InputName.cs
@@ -6,29 +6,43 @@ public class InputName : MonoBehaviour
 {
     //Variables
     public TMP_Text enterNameVar;  // text name for user
-    private TouchScreenKeyboard keyboard;  // keyboard variable
-    public GameObject pageMover;
+    public TMP_Text isYourNameConfirmName;  // confirmation of name text
+    string nameString;  // converting ui text into a string to change name
 
+    TouchScreenKeyboard keyboard;  // keyboard variable
+
+    public GameObject pageMover;  // gameobject that holds the pages, the user can move
+    public GameObject confirmNameBox;  // confirmation box pops up asking the user if they have inputted the correct name
+
+
+    // unitys start function
     private void Start()
     {
-        
+        nameString = isYourNameConfirmName.text = ("Is ' Name ' your name?").ToString();
     }
 
     // unitys update function
-    private void Update()
-    {   
+    void Update()
+    {
+        if(nameString.Contains("Name"))
+        {
+            isYourNameConfirmName.text = nameString.Replace("Name", enterNameVar.text);
+            Debug.Log("Name changed");
+        }
+
         try
         {
             if (keyboard.active)  // check to see if the keyboard is active
             {
                 enterNameVar.text = keyboard.text;  // apply whatever is inputted into the keyboard to the 'name' variable
             }
-            
-            if(keyboard.status == TouchScreenKeyboard.Status.Done)
+
+            if (keyboard.status == TouchScreenKeyboard.Status.Done)
             {
+                confirmNameBox.SetActive(true);
                 
-                Debug.Log("Done typing name _ turn to next page(TITLE)");
             }
+            else { confirmNameBox.SetActive(false); }
         }
         catch (NullReferenceException) { }
     }
@@ -37,7 +51,22 @@ public class InputName : MonoBehaviour
     public void ClickToTypeName()
     {
         keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
-        Debug.Log("Mouse clicked button");
+        Debug.Log("Touch to enter name");
+    }
+
+    // Attached to the yes confirmation button , continues to next page
+    public void YesConfirmNameButton()
+    {
         pageMover.GetComponent<Animator>().SetTrigger("canGoToTitlePage");
+        confirmNameBox.SetActive(false);
+        Debug.Log("YEs confirm name _ turn to next page(TITLE)");
+    }
+
+    // Attached to the no confirmation button, gives the user a chance to change their name
+    public void NoConfirmNameButton()
+    {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+        confirmNameBox.SetActive(false);
+        Debug.Log("Not my name return to keyboard_ turn to next page(TITLE)");
     }
 }
