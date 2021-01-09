9bcf30ee38400c64cfd5c546b3689f5ce72aa9ad
GameDev-Mark <gamedev.markd@gmail.com>
2021-01-09 12:53:36 +0100

    Scale Resolution update

--- Piboco project-01/Assets/Scripts/GameCon.cs
+++ Piboco project-01/Assets/Scripts/GameCon.cs
@@ -3,30 +3,81 @@
 public class GameCon : MonoBehaviour
 {
     //Variables
-    public GameObject pageMover;
+    public GameObject lastPage;  // last page gameobject
 
-    private void Update()
+    public RectTransform pagerMoverRectT;  // parent object of all pages
+    public RectTransform namePage;
+    public RectTransform _titlePage;
+    public RectTransform _page01;
+    public RectTransform _page02;
+    public RectTransform _page03;
+    public RectTransform _page04;
+    public RectTransform _page05;
+    public RectTransform _page06;
+
+    float scrollSpeed;
+
+    bool _firstPageBlocker;  // boolean trigger to see if the user is at first page 
+    bool _lastPageBlocker; // boolean trigger to see if the user is at last page 
+
+
+    // unitys start function
+    private void Start()
     {
-        DragPages_Turning();
+        scrollSpeed = 18f;
+
+        _titlePage.anchoredPosition = new Vector2(800f, _titlePage.anchoredPosition.y);
+        _page01.anchoredPosition = new Vector2(1600f, _page01.anchoredPosition.y);
+        _page02.anchoredPosition = new Vector2(2400f, _page02.anchoredPosition.y);
+        _page03.anchoredPosition = new Vector2(3200f, _page03.anchoredPosition.y);
+        _page04.anchoredPosition = new Vector2(4000f, _page04.anchoredPosition.y);
+        _page05.anchoredPosition = new Vector2(4800f, _page05.anchoredPosition.y);
+        _page06.anchoredPosition = new Vector2(5600f, _page06.anchoredPosition.y);
     }
 
-
-    public void DragPages_Turning()
+    // unitys update function
+    void Update()
     {
-        if(Input.GetMouseButton(0))
-        {
-            if (Input.mousePosition.x > Screen.width / 2)
-            {
-                pageMover.GetComponent<RectTransform>().Translate(Vector3.left * 3f);
-                Debug.Log("Touch and drag right side of screen");
-            }
+        NextPages();
 
+        if (pagerMoverRectT.anchoredPosition.x > Screen.width / 100)
+        {
+            _firstPageBlocker = true;
+        }
+
+        if (lastPage.transform.position.x < Screen.width / 2)
+        {
+            _lastPageBlocker = true;
+        }
+    }
+
+    // tap and hold left or right side of the screen to go back or forwards on the pages
+    void NextPages()
+    {
+        if (Input.GetMouseButton(0))
+        {
             if (Input.mousePosition.x < Screen.width / 2)
             {
-                pageMover.GetComponent<RectTransform>().Translate(Vector3.right * 3f);
-                Debug.Log("Touch and drag left side of screen");
+                pagerMoverRectT.Translate(Vector3.right * scrollSpeed);
+                _lastPageBlocker = false;
+                //Debug.Log("Touch left side of screen to move left (BACKWARDS)");
             }
-            
+            else if (Input.mousePosition.x > Screen.width / 2)
+            {
+                pagerMoverRectT.Translate(Vector3.left * scrollSpeed);
+                _firstPageBlocker = false;
+                //Debug.Log("Touch right side of screen to move right (FORWARDS)");
+            }
+
+            if (_firstPageBlocker == true) // stops the scrolling at the first page 
+            {
+                pagerMoverRectT.anchoredPosition = Vector3.zero;
+            }
+
+            if (_lastPageBlocker == true) // stops the scrolling at the last page
+            {
+                pagerMoverRectT.anchoredPosition = new Vector3(-5600f, pagerMoverRectT.anchoredPosition.y);
+            }
         }
     }
 }
--- Piboco project-01/Assets/Scripts/InputName.cs
+++ Piboco project-01/Assets/Scripts/InputName.cs
@@ -12,22 +12,48 @@ public class InputName : MonoBehaviour
 
     public GameObject pageMover;  // gameobject that holds the pages, the user can move
     public GameObject confirmNameBox;  // confirmation box pops up asking the user if they have inputted the correct name
-    GameObject[] userName;
+    GameObject[] userName;  // collecting an array of gameobjects and naming them userName
+
 
     // unitys start function
-    private void Start()
+    void Start()
     {
         userName = GameObject.FindGameObjectsWithTag("Name");
 
     }
 
-
     // unitys update function
     void Update()
     {
+        try
+        {
+            if (keyboard.active)  // check to see if the keyboard is active
+            {
+                enterNameVar.text = keyboard.text;  // apply whatever is inputted into the keyboard to the 'name' variable
+                isYourNameConfirmName.text = keyboard.text;
+            }
+
+            if (keyboard.status == TouchScreenKeyboard.Status.Done)
+            {
+                confirmNameBox.SetActive(true);
+            }
+        }
+        catch (NullReferenceException) { }
+    }
+
+    // attached event to the name button , tap / press screen space to enter your name , prompt keyboard
+    public void ClickToTypeName()
+    {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+        //Debug.Log("Touch to enter name");
+    }
+
+    // Attached to the yes confirmation button , continues to next page
+    public void YesConfirmNameButton()
+    {
         if (userName.Length >= 0)
         {
-            // FIND ALL TMP_Text COMPONENTS AND REPLACE 'NAME' IN THEM TO THE USER'S ENTERED NAME
+            // FIND ALL TextMeshProUGUI COMPONENTS AND REPLACE 'NAME' IN  THE TEXT TO THE USER'S ENTERED NAME
             foreach (GameObject name in userName)
             {
                 name.GetComponent<TextMeshProUGUI>().text = name.GetComponent<TextMeshProUGUI>().text.Replace("Name", enterNameVar.text);
@@ -35,37 +61,8 @@ public class InputName : MonoBehaviour
                 Debug.Log("CHECK name change start " + userName.Length);
             }
         }
-
-        try
-        {
-            if (keyboard.active)  // check to see if the keyboard is active
-            {
-                enterNameVar.text = keyboard.text;  // apply whatever is inputted into the keyboard to the 'name' variable
-            }
-
-            if (keyboard.status == TouchScreenKeyboard.Status.Done)
-            {
-                confirmNameBox.SetActive(true);
-                
-            }
-            else { confirmNameBox.SetActive(false); }
-        }
-        catch (NullReferenceException) { }
-    }
-
-    // tap / press screen space to enter your name // prompt keyboard
-    public void ClickToTypeName()
-    {
-        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
-        Debug.Log("Touch to enter name");
-    }
-
-    // Attached to the yes confirmation button , continues to next page
-    public void YesConfirmNameButton()
-    {
-        pageMover.GetComponent<Animator>().SetTrigger("canGoToTitlePage");
         confirmNameBox.SetActive(false);
-        Debug.Log("YEs confirm name _ turn to next page(TITLE)");
+        //Debug.Log("YEs confirm name _ turn to next page(TITLE)");
     }
 
     // Attached to the no confirmation button, gives the user a chance to change their name
@@ -73,6 +70,6 @@ public class InputName : MonoBehaviour
     {
         keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
         confirmNameBox.SetActive(false);
-        Debug.Log("Not my name return to keyboard_ turn to next page(TITLE)");
+        //Debug.Log("Not my name return to keyboard_ turn to next page(TITLE)");
     }
 }
