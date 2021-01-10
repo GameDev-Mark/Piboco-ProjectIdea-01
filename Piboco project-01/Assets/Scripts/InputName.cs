2cbeee1d2ca78fb7835c543713ef9f9b3191e814
GameDev-Mark <gamedev.markd@gmail.com>
2021-01-10 18:38:05 +0100

    PC keybord input

--- Piboco project-01/Assets/Scripts/InputName.cs
+++ Piboco project-01/Assets/Scripts/InputName.cs
@@ -7,12 +7,14 @@ public class InputName : MonoBehaviour
     //Variables
     public TMP_Text enterNameVar;  // text name for user
     public TMP_Text isYourNameConfirmName;  // confirmation of name text
+    
 
     public RectTransform pageMoverAll;  // transform that holds the pages, the user can move
 
-    TouchScreenKeyboard keyboard;  // keyboard variable
+    /*TouchScreenKeyboard keyboard;*/  // ANDROID AND IPHONE keyboard //
+    public GameObject _keyboardPC;  // PC keyboard //
 
-    bool _isFinshedName;  // boolean stating when name has been entered in by the user
+    /*bool _isFinshedName; */ // ANDROID AND IPHONE keyboard boolean stating when name has been entered in by the user // 
 
     public GameObject confirmNameBox;  // confirmation box pops up asking the user if they have inputted the correct name
     GameObject[] userName;  // collecting an array of gameobjects and naming them userName
@@ -29,28 +31,45 @@ public class InputName : MonoBehaviour
     {
         try
         {
-            if (keyboard.active)  // check to see if the keyboard is active
-            {
-                enterNameVar.text = keyboard.text;  // apply whatever is inputted into the keyboard to the 'name' variable
-                isYourNameConfirmName.text = "Is " + keyboard.text + " your name?";
-            }
+            // MOBILE ANDROID AND IPHONE KEYBOARD PROMPTING //
+            //if (keyboard.active)  // check to see if the keyboard is active
+            //{
+            //    enterNameVar.text = keyboard.text;  // apply whatever is inputted into the keyboard to the 'name' variable
+            //    isYourNameConfirmName.text = "Is " + keyboard.text + " your name?";
+            //}
 
-            if (keyboard.status == TouchScreenKeyboard.Status.Done && _isFinshedName == false)
+            //if (keyboard.status == TouchScreenKeyboard.Status.Done && _isFinshedName == false)
+            //{
+            //    confirmNameBox.SetActive(true);
+            //}
+            //else if (keyboard.status == TouchScreenKeyboard.Status.Done && _isFinshedName == true)
+            //{
+            //    confirmNameBox.SetActive(false);
+            //}
+
+            // PC KEYBOARD PROMPTING // 
+            if (_keyboardPC.gameObject.activeInHierarchy)  // check to see if the keyboard is active
             {
-                confirmNameBox.SetActive(true);
-            }
-            else if (keyboard.status == TouchScreenKeyboard.Status.Done && _isFinshedName == true)
-            {
-                confirmNameBox.SetActive(false);
+                enterNameVar.text = _keyboardPC.GetComponentInChildren<TMP_InputField>().text;  // apply whatever is inputted into the keyboard to the 'name' variable
+                isYourNameConfirmName.text = "Is " + enterNameVar.text + " your name?";
             }
         }
         catch (NullReferenceException) { }
     }
 
+    // attached to the enter button , confirm name at the start // PC KEYBOARD //
+    public void EnterButton()
+    {
+        _keyboardPC.gameObject.SetActive(false);
+        confirmNameBox.SetActive(true);
+    }
+
     // attached event to the name button , tap / press screen space to enter your name , prompt keyboard
     public void ClickToTypeName()
     {
-        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+        //keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);  // ANDROID / IPHONE KEYBOARD PROMPT //
+
+        _keyboardPC.gameObject.SetActive(true);  // activate input text field as keyboard / PC KEYBOARD //
         AudioSource.PlayClipAtPoint(GetComponent<GameCon>().popSFX, Camera.main.transform.position);
         //Debug.Log("Touch to enter name");
     }
@@ -65,13 +84,13 @@ public class InputName : MonoBehaviour
             {
                 name.GetComponent<TextMeshProUGUI>().text = name.GetComponent<TextMeshProUGUI>().text.Replace("Name", enterNameVar.text);
                 name.GetComponent<TextMeshProUGUI>().text = name.GetComponent<TextMeshProUGUI>().text.Replace("Names", enterNameVar.text + "s");
-                Debug.Log("CHECK name change start " + userName.Length);
+                //Debug.Log("CHECK name change start " + userName.Length);
             }
         }
 
         pageMoverAll.anchoredPosition = new Vector2(-800f, pageMoverAll.anchoredPosition.y);
         GetComponent<UserTouchMovement>().enabled = true;
-        _isFinshedName = true;
+        //_isFinshedName = true;  // bool for android and iphone keyboard //
         confirmNameBox.SetActive(false);
         AudioSource.PlayClipAtPoint(GetComponent<GameCon>().popSFX, Camera.main.transform.position);
         //Debug.Log("YEs confirm name _ turn to next page(TITLE)");
@@ -80,7 +99,9 @@ public class InputName : MonoBehaviour
     // Attached to the no confirmation button, gives the user a chance to change their name
     public void NoConfirmNameButton()
     {
-        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+        //keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);  // ANDROID / IPHONE KEYBOARD PROMPT // 
+
+        _keyboardPC.gameObject.SetActive(true);  // activate input text field as keyboard // PC KEYBOARD //
         confirmNameBox.SetActive(false);
         AudioSource.PlayClipAtPoint(GetComponent<GameCon>().popSFX, Camera.main.transform.position);
         //Debug.Log("Not my name return to keyboard_ turn to next page(TITLE)");
